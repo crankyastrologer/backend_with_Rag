0b4bb7a2875063341ee639fad4c1a0cbd420471e
crankyastrologer <ansh@ansh-pc>
2024-07-08 19:28:30 +0530

    commit

--- RAG_chain/llm.py
+++ RAG_chain/llm.py
@@ -1,17 +1,21 @@
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough
-from langchain_google_genai import ChatGoogleGenerativeAI
 from .document_loader import document_loader
-
-
+from langchain_community.llms.cloudflare_workersai import CloudflareWorkersAI
+import os
 def load():
-    llm = ChatGoogleGenerativeAI(model='gemini-pro')
+    llm = CloudflareWorkersAI(account_id=os.environ['ACCOUNT_ID'],api_token = os.environ['API_TOKEN'],model='@cf/qwen/qwen1.5-14b-chat-awq')
     vectorstore, format_docs = document_loader()
     retriever = vectorstore.as_retriever(search_kwargs={'k': 6})
     prompt = PromptTemplate(
-        template="""You are an assistant for question-answering tasks regarding ansh's abilities. Use the following pieces of retrieved context about ansh  to answer the question. If you don't know the answer, just say that you don't know. The answer should be detailed.\nQuestion: {question} \nContext: {context} \nAnswer:""",
+        template="""[INST]<<SYS>> You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise. Answer in a natural way like a human would and dont mention that you retreived it from context<</SYS>> 
+Question: {question} 
+Context: {context} 
+Answer: [/INST]""",
         input_variables=['context', 'question'])
+    print(llm.model)
+    print(prompt.template)
     rag_chain = (
             {"context": retriever | format_docs, "question": RunnablePassthrough()}
             | prompt
@@ -23,4 +27,5 @@ def load():
 def getResponse(question,rag_chain):
 
     ans = rag_chain.invoke(question)
-    return ans;
+    print(ans)
+    return ans
--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from init import cross_origin,app,request,jsonify
 from RAG_chain import load,getResponse
+import time
 rag_chain = load()
 @app.route('/api', methods=['POST','OPTIONS','GET'])
 @cross_origin()
@@ -7,16 +8,21 @@ def getans():
     data = request.get_json()
     print(data)
     if 'question' in data:
-        question = str(data['question'])
-        ans = getResponse(question,rag_chain)
-        print(question)
-        response = {
-            'response': f'{ans}'
-        }
-        response = jsonify(response)
-
-        return response
-
+        try:
+            question = str(data['question'])
+            
+            ans = getResponse(question,rag_chain)
+            print(question)
+            
+            response = {
+                'response': f'{ans}'
+            }
+            response = jsonify(response)
+            
+            return response
+        except Exception as e:
+            print(e)
+            return 402, "Something went wrong"
     else:
         return 403, "Question not found"
 
